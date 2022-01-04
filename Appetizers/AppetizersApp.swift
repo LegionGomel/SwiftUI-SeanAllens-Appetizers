e9204dd2c22a51eb63933596e2ccaad01c0858fa
Yuri Ershov <shtorm0606@gmail.com>
2022-01-04 19:46:45 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Yuri Ershov on 4.01.22.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
