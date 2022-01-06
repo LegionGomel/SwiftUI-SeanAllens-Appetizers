4b0a216a86c4f3e648d1a91634c0c5c35ae53b66
Yuri Ershov <shtorm0606@gmail.com>
2022-01-06 19:40:49 +0300

    Add viewModel for listView and make some code refactoring to MVVM

---
+++ Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  AppetizerListViewModel.swift
+//  Appetizers
+//
+//  Created by Yuri Ershov on 6.01.22.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    
+    @Published var appetizers: [Appetizer] = []
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+}
