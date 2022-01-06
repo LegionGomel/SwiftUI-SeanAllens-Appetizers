4a5ed78ac205dbf74e125376e0c46b4db8d675ba
Yuri Ershov <shtorm0606@gmail.com>
2022-01-06 20:28:41 +0300

    Add proper errors handling and show them in an Alert popup

--- Appetizers/AppetizerListViewModel.swift
+++ Appetizers/AppetizerListViewModel.swift
@@ -10,15 +10,32 @@ import SwiftUI
 final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
-        NetworkManager.shared.getAppetizers { result in
+        // To not to use everytime self word (self.alettItem, for example)
+        // we can use [self] in closures
+        NetworkManager.shared.getAppetizers { [self] result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let appetizers):
+                    // here we MUST use self because of same naming
                     self.appetizers = appetizers
+                    
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidResponce:
+                        alertItem = AlertContext.invalidResponce
+                        
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                        
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                        
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
