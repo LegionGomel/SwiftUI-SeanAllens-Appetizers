63032e3186de4b58465c4092f5a0d3b7d092ca8c
Yuri Ershov <shtorm0606@gmail.com>
2022-01-07 09:49:27 +0300

    Get to use remote image instead of test image in assets

--- Appetizers/AppetizerListCell.swift
+++ Appetizers/AppetizerListCell.swift
@@ -13,15 +13,16 @@ struct AppetizerListCell: View {
     
     var body: some View {
         HStack {
-            Image("asian-flank-steak")
-                .resizable()
+            AppetizerRemoteImage(urlString: appetizer.imageURL)
                 .scaledToFit()
                 .frame(width: 120, height: 90)
                 .cornerRadius(8)
+            
             VStack(alignment: .leading, spacing: 5) {
                 Text(appetizer.name)
                     .font(.title2)
                     .fontWeight(.medium)
+                
                 Text("$ \(appetizer.price, specifier: "%.2f")")
                     .foregroundColor(.secondary)
                     .fontWeight(.semibold)
