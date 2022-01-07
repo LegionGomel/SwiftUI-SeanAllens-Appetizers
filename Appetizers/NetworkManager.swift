34bfa6a0569fdf749811a9e7e7616789a7a6d623
Yuri Ershov <shtorm0606@gmail.com>
2022-01-07 09:27:47 +0300

    Add a function to get an image (from network call or cache)

--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -5,13 +5,14 @@
 //  Created by Yuri Ershov on 6.01.22.
 //
 
-import Foundation
+import UIKit
 
 
 // Singleton
 final class NetworkManager {
     
     static let shared =  NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let  baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
@@ -19,7 +20,7 @@ final class NetworkManager {
     private init() {}
     
     
-    /* Once ttat is completed, we have a Result. It have an array of appetizers, or an
+    /* Once that is completed, we have a Result. It have an array of appetizers, or an
      Error in a failure case.
      */
     func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
@@ -59,6 +60,39 @@ final class NetworkManager {
             }
         }
         
+        task.resume()
+    }
+    
+    
+    func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        // Checking a cache to see if we have an object(image)
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        // Ckeck for a valid URL from string
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        // if no image in cache, create task from URLSession to get image
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, responce, error in
+            
+            // Check for a good data
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            // If everything OK, set the image in a cache
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+        }
         task.resume()
     }
 }
