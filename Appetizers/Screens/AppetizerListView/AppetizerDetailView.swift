7dfd64c925dfa922e333fe5bff004d2203230bd0
Yuri Ershov <shtorm0606@gmail.com>
2022-01-07 10:24:27 +0300

    Add description card

---
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -0,0 +1,93 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizers
+//
+//  Created by Yuri Ershov on 7.01.22.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
+    
+    var body: some View {
+        VStack {
+            Image("asian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 225, alignment: .top)
+            VStack {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                
+                HStack(spacing: 40) {
+                    VStack(spacing: 5) {
+                        Text("Calories")
+                            .font(.caption)
+                            .bold()
+                            .fontWeight(.bold)
+                        
+                        Text("\(appetizer.calories)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Carbs")
+                            .font(.caption)
+                            .bold()
+                            .fontWeight(.bold)
+                        
+                        Text("\(appetizer.carbs)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Proteines")
+                            .font(.caption)
+                            .bold()
+                            .fontWeight(.bold)
+                        
+                        Text("\(appetizer.protein)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                }
+            }
+            Spacer()
+            Button {
+                print("tapped")
+            } label: {
+                Text("Button")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .frame(width: 260, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.brandPrimary)
+                    .cornerRadius(10)
+            }
+            .padding(.bottom, 30)
+        }
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
+    }
+}
+
+struct AppetizerDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+    }
+}
