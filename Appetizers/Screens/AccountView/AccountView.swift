be83d3d1e854574458690a984f57377327bbe1a8
Yuri Ershov <shtorm0606@gmail.com>
2022-01-07 17:27:59 +0300

    Finish UI for first section in an account view

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    
     var body: some View {
         NavigationView {
-            Text("Account view")
-                .navigationTitle("Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    Button {
+                        print("save")
+                    } label: {
+                        Text("Save changes")
+                    }
+                } header: {
+                    Text("Personal Info")
+                }
+            }
+            .navigationTitle("🤣 Account")
         }
     }
 }
